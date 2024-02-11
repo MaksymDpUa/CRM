4ced520354c771084708b361645a4a8137164c39
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2024-02-11 11:57:53 +0100

    add some components

---
+++ src/app/components/Logo-uploader.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react';
+
+export interface LogoUploaderProps
+  extends Partial<Omit<React.ReactHTMLElement<HTMLInputElement>, 'type'>> {
+  label?: string;
+  placeholder?: string;
+  id?: string;
+}
+
+export default function LogoUploader({
+  label,
+  placeholder,
+  id,
+  ...rest
+}: LogoUploaderProps) {
+  return (
+    <div className="flex gap-10 mb-13">
+      {label && <p className="text-base color-gray-900">{label}</p>}
+      <label
+        htmlFor={id}
+        className="flex flex-col items-center justify-center w-40 h-40 bg-white border border-slate-900 border-dashed rounded-full"
+      >
+        <img
+          className="mb-1"
+          width={48}
+          height={48}
+          src="/src/icons/upload.svg"
+          alt="upload"
+        />
+        {placeholder && (
+          <p className="text-base color-gray-500">{placeholder}</p>
+        )}
+        <input
+          {...rest}
+          id={id}
+          type="file"
+          accept="image/*"
+          className="hidden"
+        />
+      </label>
+    </div>
+  );
+}
